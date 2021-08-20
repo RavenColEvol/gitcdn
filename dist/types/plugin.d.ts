f68080ed1d20e19157fb7d8e94056f9a7c61970c
Ravi Lamkoti <ravilamkoti@gmail.com>
2021-08-20 22:13:56 +0530

    initial commit

---
+++ dist/types/plugin.d.ts
@@ -0,0 +1,7 @@
+declare const _default: any;
+export default _default;
+/**
+ * https://www.youtube.com/watch?v=NVphWvObKL8
+ * https://twitter.com/desmondprograms/status/1427919826081751045
+ *
+*/ 
